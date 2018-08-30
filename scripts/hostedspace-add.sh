826974980a2893d6f4d3a5cfbd062b2188ab8c59
n-peugnet <n.peugnet@free.fr>
2018-08-31 00:36:49 +0200

    Options parsing and Usage for hostedspace-add

--- scripts/hostedspace-add.sh
+++ scripts/hostedspace-add.sh
@@ -1,6 +1,45 @@
-if [ $# > 0 ]
-then
-	username=$1
+#!/bin/bash
+
+usage() {
+	echo "Usage:"
+	echo "  hostedspace-add [name] [options]"
+	echo ""
+	echo "Options:"
+	echo "  -h               Show help."
+	echo "  -s               With shell."
+	echo "  -p <password>    Set the password."
+	exit 0
+}
+
+getOptions() {
+	while getopts "hsp:" opt; do
+		case $opt in
+		h)
+			usage
+			;;
+		s)
+			echo "With shell."
+			;;
+		p)
+			echo "Set the password: $OPTARG"
+			;;
+		esac
+	done
+	lastopt=$((OPTIND - 1))
+	OPTIND=1
+}
+
+params=()
+
+if [ $# = 0 ]; then
+	usage
+else
+	while [ $# -gt 0 ]; do
+		getOptions $@
+		shift $lastopt
+		params+=($1)
+		shift
+	done
+	echo ${params[*]}
+	#useradd $username
 fi
-echo $username
-#useradd $username
