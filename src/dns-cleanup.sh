a3761ebf769ac185bc90ce6b10e7a7ca9852d684
n-peugnet <n.peugnet@free.fr>
2021-11-02 12:24:32 +0100

    dns cleanup add mail + move sed to last challenge

--- src/dns-cleanup.sh
+++ src/dns-cleanup.sh
@@ -10,12 +10,18 @@ HOST="_acme-challenge"
 ZONE="/etc/bind/db.$CERTBOT_DOMAIN"
 DATE=$(date +%Y%m%d%H)
 
-sed $ZONE -Ei -e "/^$HOST.*$VALIDATION/d"
-
-dns-bump $ZONE
 
 if [[ $CERTBOT_REMAINING_CHALLENGES == 0 ]]
 then
+	sed $ZONE -Ei -e "/^$HOST.*$VALIDATION/d"
+	dns-bump $ZONE
 	rndc reload
 	systemctl restart bind9
 fi
+
+printf "Result for $CERTBOT_DOMAIN\n
+validation:	$CERTBOT_VALIDATION
+remaining:	$CERTBOT_REMAINING_CHALLENGES
+output:
+$CERTBOT_AUTH_OUTPUT
+" | mailx -s "Certbot renewal result for $CERTBOT_DOMAIN" root
