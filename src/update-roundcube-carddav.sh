7c0c7fb7bbe9cd919dd8207955c8cf3a486befe1
n-peugnet <n.peugnet@free.fr>
2022-02-11 22:19:17 +0100

    feat: add update-roundcube-carddav script

---
+++ src/update-roundcube-carddav.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+DIR="${BASH_SOURCE%/*}"
+. "$DIR/../lib/club1/functions.sh"
+
+# From config
+repo=mstilkerich/rcmcarddav
+cwd=/var/lib/roundcube/plugins
+
+tag=$(getLatestGithubRelease $repo)
+dir=carddav
+tar=$dir-$tag.tar.gz
+url=https://github.com/$repo/releases/download/$tag
+
+cd $cwd
+
+wget -nv $url/$tar
+rm -rf $dir
+tar -xf $tar
+ln -s /etc/roundcube/plugins/carddav/config.inc.php $dir/
+echo success
+rm $tar
