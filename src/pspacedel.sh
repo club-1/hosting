ffc7caaf1ee4c393cbb27400747e647a25c0c698
Nicolas Peugnet <nicolas@club1.fr>
2023-04-19 17:37:27 +0200

    feat: delete all by default, add option to keep fpm pool

--- src/pspacedel.sh
+++ src/pspacedel.sh
@@ -8,8 +8,9 @@ usage() {
 	echo ""
 	echo "Options:"
 	echo "  -h    Show help."
-	echo "  -r    Remove home directory"
-	echo "  -m    Remove MariaDb MySql account."
+	echo "  -r    Keep home directory"
+	echo "  -m    Keep MariaDb MySql account."
+	echo "  -f    Keep FPM pool"
 	echo "  -g    Keep group."
 	echo "  -q    Quiet."
 	exit 0
@@ -24,8 +25,9 @@ loginGet
 verbose "userdel $login"
 ldapdeleteuser $login
 
-[[ -n ${options[r]} ]] && homeDel
-[[ -n ${options[m]} ]] && sqlUserDel
+[[ -z ${options[r]} ]] && homeDel
+[[ -z ${options[m]} ]] && sqlUserDel
+[[ -z ${options[f]} ]] && phpfpmpoolDel
 [[ -z ${options[g]} ]] && groupDel
 
 exit 0
